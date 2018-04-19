c937d0187f884d488837faf8c94a12cc629e6917
Administrator <admin@example.com>
2018-04-19 22:02:43 +0800

    smart-framework - MVC Framework Works

---
+++ smart-framework/src/main/java/cn/foreversong/snail/sf/DispatcherServlet.java
@@ -0,0 +1,121 @@
+package cn.foreversong.snail.sf;
+
+import cn.foreversong.snail.sf.bean.Data;
+import cn.foreversong.snail.sf.bean.Handler;
+import cn.foreversong.snail.sf.bean.Param;
+import cn.foreversong.snail.sf.bean.View;
+import cn.foreversong.snail.sf.helper.BeanHelper;
+import cn.foreversong.snail.sf.helper.ConfigHelper;
+import cn.foreversong.snail.sf.helper.ControllerHelper;
+import cn.foreversong.snail.sf.util.*;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Method;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 长歌
+ * Date: 2018/4/19
+ * Time: 21:17
+ * Description: 请求转发器
+ */
+public class DispatcherServlet extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        // 初始化相关Helper类
+        BootStrap.init();
+        // 获取ServletContext对象(用于注册Servlet)
+        ServletContext servletContext = config.getServletContext();
+        //注册处理 jsp 的Servlet
+        ServletRegistration jspServlet = servletContext.getServletRegistration("jsp");
+        jspServlet.addMapping(ConfigHelper.getAppJspPath() + "*");
+        // 注册处理静态资源的默认Servlet
+        ServletRegistration defaultServlet = servletContext.getServletRegistration("default");
+        defaultServlet.addMapping(ConfigHelper.getAppAssetPath() + "*");
+
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 获取请求方法与请求路径
+        String requestMethod = req.getMethod().toLowerCase();
+        String requestPath = req.getPathInfo();
+        // 获取 Action 处理器
+        Handler handler = ControllerHelper.getHandler(requestMethod, requestPath);
+        if (handler != null) {
+            // 获取 Controller 类及其 Bean 实例
+            Class<?> controllerClass = handler.getControllerClass();
+            Object controllerBean = BeanHelper.getBean(controllerClass);
+            // 创建请求参数对象
+            Map<String, Object> paramMap = new HashMap<String, Object>();
+            Enumeration<String> paramNames = req.getHeaderNames();
+            while (paramNames.hasMoreElements()) {
+                String paramName = paramNames.nextElement();
+                String paramValue = req.getParameter(paramName);
+                paramMap.put(paramName, paramValue);
+            }
+            String body = CodecUtil.decodeURL(StreamUtil.getString(req.getInputStream()));
+            if (StringUtil.isNotEmpty(body)) {
+                String[] params = StringUtil.splitString(body, "&");
+                if (ArrayUtil.isNotEmpty(params)) {
+                    for (String param : params) {
+                        String[] array = StringUtil.splitString(param, "=");
+                        if (ArrayUtil.isNotEmpty(array) && array.length == 2) {
+                            String paramName = array[0];
+                            String paramValue = array[1];
+                            paramMap.put(paramName, paramValue);
+                        }
+                    }
+                }
+            }
+            Param param = new Param(paramMap);
+            Method actionMethod = handler.getActionMethod();
+            Object result = ReflectionUtil.invokeMethod(controllerBean, actionMethod, param);
+            // 调用 Action 方法
+            if (result instanceof View) {
+                // 返回 Jsp 页面
+                View view = (View) result;
+                String path = view.getPath();
+                if(StringUtil.isNotEmpty(path)){
+                    if(path.startsWith("/")){
+                        resp.sendRedirect(req.getContextPath() + path);
+                    }else {
+                        Map<String , Object> model = view.getModel();
+//                        model.forEach((Map.Entry<String,Object> entry)->{
+//                            req.setAttribute(entry.getKey(),entry.getValue());
+//                        });
+                        model.entrySet().forEach((entry -> {
+                            req.setAttribute(entry.getKey(),entry.getValue());
+                        }));
+                        req.getRequestDispatcher(ConfigHelper.getAppJspPath() + path).forward(req, resp);
+                    }
+                }
+            }else if (result instanceof Data){
+                // 返回 JSON 数据
+                Data data = (Data) result;
+                Object model = data.getModel();
+                if(model != null){
+                    resp.setContentType("application/json");
+                    resp.setCharacterEncoding("UTF-8");
+                    PrintWriter writer = resp.getWriter();
+                    String json = JsonUtil.toJson(model);
+                    writer.write(json);
+                    writer.flush();
+                    writer.close();
+                }
+            }
+
+        }
+    }
+}
